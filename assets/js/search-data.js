e0918b4c334e02b5fc9181e24ea5973d4e892877
kfoysalhaque <haque.k@northeastern.edu>
2025-01-20 22:58:56 +0000

    Deploying to gh-pages from @ kfoysalhaque/kfoysalhaque.github.io@22ac293e0eebe08fda6d72af2010f04b3942e6aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -509,13 +509,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/khandaker-foysal-haque-53619b81", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
