388b8d63806b65b979ac33133f714f77464aac1b
kfoysalhaque <haque.k@northeastern.edu>
2025-02-01 21:41:37 +0000

    Deploying to gh-pages from @ kfoysalhaque/kfoysalhaque.github.io@3bd1017f4feaac28604dddd7113d5b2fa5cc2e25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,16 +396,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-beamsense-has-been-accepted-for-publication-in-computer-networks-if-4-4",
+          title: 'Our paper BeamSense has been accepted for publication in Computer Networks (IF: 4.4)...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
